1f60d5b2f2ca4769e55853248017b93b51f3189e
Elbert <80282612+elbertsilva@users.noreply.github.com>
2025-01-18 16:34:00 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,7 @@
+function btCard1() {
+  //  alert('oi')
+  window.location.href = 'https://elbertsilva.github.io/Loja-de-games/';
+};
+function btCard2() {
+    window.location.href = 'https://elbertsilva.github.io/Jls-sport/';
+  };
